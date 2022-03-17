56a605b71a086b901e87907e62bb2f9ce258fd7f
Vu Le <vulevu121@gmail.com>
2022-03-17 00:38:00 -0700

    Fixed install script typo

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt install autoconf libtool libudev-dev gcc g++ make cmake unzip libxml2-dev
-sed -i 's/"quiet splash"/"quiet splash usbcore.usbfs_memory_mb=150"/g' /etc/default/grub
-sudo upgrate-grub
+sudo sed -i 's/"quiet splash"/"quiet splash usbcore.usbfs_memory_mb=150"/g' /etc/default/grub
+sudo update-grub
 echo "Please reboot system"
